7537fbacac00b1eec1ce7c5c1908ad823700c05d
Grativendas <grativendas100@gmail.com>
2026-09-22 18:00:37 -0300

    Grativendas Ver 1.3.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "783eafb008d51b4ea2a1a7be3a738c5d",
-"index.html": "3ef996de9200dd9d876d26ead6199c66",
-"/": "3ef996de9200dd9d876d26ead6199c66",
+"index.html": "e457c3b30b06ea7c3cb0a863ea3a7735",
+"/": "e457c3b30b06ea7c3cb0a863ea3a7735",
 "firebase-messaging-sw.js": "e2af1d0a99f9fe7a4233ba79a4743fcd",
-"main.dart.js": "fbfd71825b8d9dda35b4da16ae2797b0",
+"main.dart.js": "0b150f9307f6544f3023996ce6557e73",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "e90ece37d10db7eda0e1877efb3b8548",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
